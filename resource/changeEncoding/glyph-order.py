807437e8dc2c9c9ec72c95657bcb788cf3e164c1
Li.M <ecori@qq.com>
2023-06-30 18:01:22 +0800

    reEncoding

--- resource/changeEncoding/glyph-order.py
+++ resource/changeEncoding/glyph-order.py
@@ -1,13 +1,17 @@
 import json
 
-with open('modern-manju-reconfig-changeencodings-try-glyphs.json') as f:
+with open("glyphs-encoding.json") as f:
     cs = json.load(f)
 
 for glyph in cs:
-    glyph['Encoding'][0] = glyph['Encoding'][0] + 8
-    glyph['Encoding'][2] = glyph['Encoding'][0]
-    if 'Refer' in glyph:
-        glyph["Refer"][0] = glyph["Refer"][0] + 8
+    res_id = glyph['Encoding'][0]
+
+    if res_id > 15:
+        glyph['Encoding'][0] = res_id + 8
+        glyph['Encoding'][2] = res_id + 8
+        if 'Refer' in glyph:
+            if glyph["Refer"][0] > 15:
+                glyph["Refer"][0] = glyph["Refer"][0] + 8
 
 with open('res.json', 'w') as f:
     json.dump(cs, f)
